86be27f96a65ea5633eba2db339d6829d775b9cb
Robert Lyn <lenny4422@gmail.com>
2022-08-03 22:23:42 -0400

    Set mediator in base API class

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,25 +1,15 @@
 ﻿using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseAPIController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator) 
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet] 
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
--- API/Controllers/BaseAPIController.cs
+++ API/Controllers/BaseAPIController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -6,5 +8,11 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseAPIController : ControllerBase
     {
+        protected readonly IMediator Mediator;
+
+        public BaseAPIController()
+        {
+            Mediator = HttpContext.RequestServices.GetService<IMediator>()!;
+        }
     }
 }
