9ff662560b330140a0c682a3ea155bb427d8d7e6
Robert Lyn <lenny4422@gmail.com>
2022-08-03 22:17:52 -0400

    Use MediatR to query activities

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
 
 namespace API
 {
@@ -45,6 +46,8 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
                 });
             });
+
+            services.AddMediatR(typeof(Application.Activities.List.Handler).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
